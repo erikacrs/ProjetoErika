67a176dba593533960f7289488479cad75f06849
Erika Cristina Rodrigues de Souza <erikas@ciandt.com>
2026-04-22 18:18:11 -0300

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,52 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('login com sucesso', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').fill('erikacrs13@gmail.com');
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login realizado' })).toBeVisible();
+ });
+
+ test('login com e-mail inválido', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').fill('teste.teste');
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+ });
+
+  test('login com senha inválida', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').fill('erikacrs13@gmail.com');
+  await page.locator('#password').fill('12345')
+  await page.getByRole('button', { name: 'login' }).click();
+ });
+
+  test('login com e-mail e senha vazio', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').fill('');
+  await page.locator('#password').fill('');
+  await page.getByRole('button', { name: 'login' }).click();
+ });
